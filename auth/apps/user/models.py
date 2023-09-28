4370d686fdd9cb5bf18c87fcaa60504c41f64e2e
Frank <pandidrums@gmail.com~>
2023-09-28 16:03:31 -0300

    commit 008 - final UserAccount methods

--- auth/apps/user/models.py
+++ auth/apps/user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-import re
 from slugify import slugify
+import json,uuid,re
 
 class UserAccountManager(BaseUserManager):
 
@@ -17,6 +17,7 @@ class UserAccountManager(BaseUserManager):
     
         if not email():
             raise ValueError('User must have a email address')
+        
         # Lower case al email
         email = self.normalize_email(email)
         extra_fields['slug'] = create_slug(extra_fields['username'])
@@ -26,6 +27,58 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self,email,password,**extrafields):
+
+        user=self.create_user(email, password,**extrafields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.role = "Admin"
+        user.verified = True
+        user.save(using=self.db)
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_active = models.BooleanField(default=True)
+    is_online = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    objects = UserAccountManager()
+
+    # Login
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    roles = (
+        ('customer', 'Customer'),
+        ('seller', 'Seller'),
+        ('admin', 'Admin'),
+        ('moderator', 'Moderator'),
+        ('helper', 'Helper'),
+        ('editor', 'Editor'),
+        ('owner', 'Owner'),
+    )
+
+    role = models.CharField(max_length=20, choices=roles, default='customer')
+    verified = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        counter = 1
+        while UserAccount.objects.filter(slug=self.slug).exists():
+            self.slug = f"{self.slug}-{counter}"
+            counter += 1
+        super().save(*args, **kwargs)
 
     
 
